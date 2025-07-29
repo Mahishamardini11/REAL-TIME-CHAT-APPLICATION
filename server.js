fb4337362617d4216c09afe7fcdb49a99c732a2c
Mahishamardini11 <154457071+Mahishamardini11@users.noreply.github.com>
2025-07-29 19:53:09 +0530

    updated

---
+++ server.js
@@ -0,0 +1,42 @@
+const WebSocket = require('ws');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = '.' + req.url;
+  if (filePath === './') filePath = './index.html';
+
+  const ext = path.extname(filePath);
+  let contentType = 'text/html';
+
+  if (ext === '.js') contentType = 'text/javascript';
+  else if (ext === '.css') contentType = 'text/css';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("Not found");
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    // Broadcast to all clients
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
